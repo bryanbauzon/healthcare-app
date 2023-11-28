14a12aa3d41e8e2ac9a6565b201b3c5a6b8595c1
Max Bryan Bauzon <mrbryanbauzon@gmail.com>
2023-11-28 15:36:56 +0800

    added forms.dart(screen)

---
+++ lib/screens/forms.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Forms extends StatefulWidget {
+  const Forms({super.key});
+
+  @override
+  State<Forms> createState() => _FormsState();
+}
+
+class _FormsState extends State<Forms> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Form")),
+    );
+  }
+}
