3d5df11fb7ef0dfa00bea3128bcdad87a8522fcd
Max Bryan Bauzon <mrbryanbauzon@gmail.com>
2023-11-28 15:42:10 +0800

    added forms

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,5 +1,7 @@
 class StringConstants {
   static const String appName = 'Holy Trinity Healthcare';
+
+  static const String forms = 'Forms';
   static const String saveForNow = 'Save For Now';
   static const String submit = 'Submit';
 }
