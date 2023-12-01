57f1fe25164183772939443a8b2ab24ff1d2b19c
Max Bryan Bauzon <mrbryanbauzon@gmail.com>
2023-12-01 14:02:13 +0800

    Restrict orientation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:holy_trinity_healthcare/screens/splash.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MaterialApp(home: MainApp()));
 }
 
