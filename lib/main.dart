23701e2817c8b577c3d50d75272763e7d4896ee3
Max Bryan Bauzon <mrbryanbauzon@gmail.com>
2024-01-07 06:45:00 +0800

    Reverted splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:holy_trinity_healthcare/screens/login.dart';
+import 'package:holy_trinity_healthcare/screens/splash.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,6 +15,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Login();
+    return const Splash();
   }
 }
